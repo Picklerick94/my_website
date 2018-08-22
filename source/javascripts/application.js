27275959986372b0bf50ab76e29b00ee0adb122a
Jasmine Tang <jastangting@gmail.com>
2018-08-22 15:46:57 +0800

    created second session

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,34 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+// $(function(){  // $(document).ready shorthand
+//   $('.work-header').fadeIn('slow');
+// });
+
+$(document).ready(function() {
+
+$(function(){  // $(document).ready shorthand
+  $('.work-header').fadeIn('slow');
+});
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+
+            const bottom_of_object = $(this).position().top + $(this).outerHeight();
+            const bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1500);
+
+            }
+
+        });
+
+    });
+
+});
