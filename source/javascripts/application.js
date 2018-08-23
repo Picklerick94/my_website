7db777db7536f44387992cc21f5506cae19c236f
Jasmine Tang <jastangting@gmail.com>
2018-08-23 18:22:19 +0800

    added favicon

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -10,25 +10,29 @@ $(document).ready(function() {
 $(function(){  // $(document).ready shorthand
   $('.work-header').fadeIn('slow');
 });
-
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
-
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
-
             const bottom_of_object = $(this).position().top + $(this).outerHeight();
             const bottom_of_window = $(window).scrollTop() + $(window).height();
-
             /* If the object is completely visible in the window, fade it */
             if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},1500);
-
+                $(this).animate({'opacity':'1'},1000);
             }
-
         });
-
     });
-
 });
+
+
+// $('.slide').on('click', function(){
+//     $('#fade-in').toggleClass('show');
+// });
+
+// $('.spin').on('click', function(){
+//     $('#spin-in').toggleClass('show2');
+// });
+
+// $('h1.spacing').on('mouseenter', function(){
+//   $(this).toggleClass('spaced');
+// });
