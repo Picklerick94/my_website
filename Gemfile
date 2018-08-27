2a51b4aa181c94db28484c931a2ef9d66901edb1
Jasmine Tang <jastangting@gmail.com>
2018-08-27 14:26:20 +0800

    updated heroku

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
+ruby "~> 2.4.0"
 
-gem 'middleman', '~> 4.2'
+gem 'middleman', '~> 4.2.1'
 gem 'middleman-autoprefixer', '~> 2.7'
 gem 'middleman-sprockets'
 gem 'middleman-deploy', git: 'https://github.com/lewagon/middleman-deploy.git'
--- config.rb
+++ config.rb
@@ -1,3 +1,9 @@
+# Use “pretty” URLs (without the `.html` suffix)
+activate :directory_indexes
+
+# Append hashes to compiled assets
+activate :asset_hash
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
@@ -20,9 +26,3 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
-
-# Use “pretty” URLs (without the `.html` suffix)
-activate :directory_indexes
-
-# Append hashes to compiled assets
-activate :asset_hash
