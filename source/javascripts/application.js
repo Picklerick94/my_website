0e2b14a0d34ef461e78ea5b2b632c506fa87c30f
Jasmine Tang <jastangting@gmail.com>
2018-08-28 21:24:17 +0800

    updated css

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -18,7 +18,7 @@ $(function(){  // $(document).ready shorthand
             const bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is completely visible in the window, fade it */
             if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'opacity':'1'},1000);
             }
         });
     });
